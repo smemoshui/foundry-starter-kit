9c2c0418d2560ccc16c40e1e4822a62cb69aaa5d
moshui <sme_moshui@outlook.com>
2023-09-01 10:21:10 +0800

    workable

--- script/generate_beta_distribution.py
+++ script/generate_beta_distribution.py
@@ -1,4 +1,5 @@
 from scipy.special import betaincinv
+from scipy.stats import beta as B
 
 alpha = 3
 beta = 3
@@ -8,17 +9,25 @@ denominator = 1e4
 numerators = []
 denominators = []
 
-for x in range(0, precision, 1):
+for x in range(0, precision + 1, 1):
     x /= precision
     y = betaincinv(alpha, beta, x)
 
-        # 拆分为分子和分母
-    x_int = int(x * precision)
+    # 拆分为分子和分母
     y_numerator = int(y * denominator)
     y_denominator = int(denominator)
     numerators.append(y_numerator)
     denominators.append(y_denominator)
-
 print(numerators)
-print(denominators)
+
+truncated_means = []
+for numerator in numerators:
+    end = numerator * 1.0 / denominator
+    if end == 0:
+        truncated_means.append(0)
+        continue
+    t_mean = B(alpha, beta, loc=0, scale=1).expect(lb=0,ub=end,conditional=True)
+    t_mean_int = int(t_mean * denominator)
+    truncated_means.append(t_mean_int)
+print(truncated_means)
     
